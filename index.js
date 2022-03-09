42c4c14287441cfa3829034f3c6a82485dbd9c2d
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-03-09 16:41:59 -0300

    Configuraçãi da API

--- index.js
+++ index.js
@@ -3,43 +3,64 @@ const { serialize } = require('v8');
 const app = express();
 
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
-const port = process.env.PORT || 3000 
+const port = process.env.PORT || 3000
 
-let numero = 4;
-
-let arr = [
-    {
-        "ns": 1,
-        "modelo": "Modelo DVR",        
-    },
-    {
-        "ns": 2,
-        "modelo": "Modelo Câmera IP"
-    },
-    {
-        "ns": 3,
-        "modelo": "Modelo NVD"
-    },
-    {
-        "ns": 4,
-        "modelo": "Modelo não Tem gerador de senha / produto não é gravador DVR/NVR nem câmera IP"
-    }
-
-]
 
 app.get('/', (req, res) => {
 
     let produto = arr.find(produto => produto.ns === numero)
-
-    
     return res.json(produto)
 })
 
+
 app.post('/poc', (req, res) => {
 
+    let numero = req.body.ns;
+
+    let arr = [
+        {
+            "ns": BCKD20000700G,
+            "modelo": "Modelo DVR",
+        },
+        {
+            "ns": BCKD42550878T,
+            "modelo": "Modelo Câmera IP"
+        },
+        {
+            "ns": VSDK78145017R,
+            "modelo": "Modelo NVD"
+        },
+        {
+            "ns": 4,
+            "modelo": "Modelo não Tem gerador de senha / produto não é gravador DVR/NVR nem câmera IP"
+        }
+    ]
+
+    let db = "Produto Cadastrado!!!"
+    let opcao = "API está ON"
+    let produto = arr.find(produto => produto.ns === numero)
+
+
+    let intelbras = {
+        "status": "success",
+        "chatbot_response": db,
+        "raw_output": [
+            {
+                "output_variable": "produto",
+                "output_result": produto
+            },
+            {
+                "output_variable": "opcao",
+                "output_result": opcao
+            }
+        ]
+    }
+
+    res.send(JSON.stringify(intelbras))
 })
+
 
 app.listen(port, () => {
     console.log('API no Heroku está ON')
