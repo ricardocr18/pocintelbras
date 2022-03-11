0fce24bad7d989282ff7001bee0836abfcb0c827
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-03-11 07:43:03 -0300

    gerando resposta da API

--- index.js
+++ index.js
@@ -45,13 +45,12 @@ app.post('/poc', (req, res) => {
     if (arr.some(confirma => confirma.ns === numero)) {
         produto = arr.find(produto => produto.ns === numero)
         db = "Produto está cadastrado em nossa base! 😉"
-        opcao = "Equipamento válido para recuperar senha"
-        modelo = produto.modelo
     } else {
         db = "Infelizmente, o número de série informado não corresponde a gravador DVR / NVR e câmera IP 😔"
     }
 
-    
+    let opcao = "Equipamento válido para recuperar senha"
+    // let produto = "Teste"
 
     let dataProduto = req.body.data
     if (dataProduto != "") {
@@ -64,7 +63,7 @@ app.post('/poc', (req, res) => {
         "raw_output": [
             {
                 "output_variable": "produto",
-                "output_result": modelo
+                "output_result": produto.modelo
             },
             {
                 "output_variable": "opcao",
