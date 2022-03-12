754276c3562efecbf5e9bc2f82ebf8d6d56dc17d
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-03-12 20:44:40 -0300

    gerando resposta de imagens na API

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.post('/poc', (req, res) => {
     
     //Gerador de senha número aleatorio
     dataProduto = req.body.data
-    if (imagem > 1) {
+    if (imagem >= 1) {
         senha = Math.floor(Math.random() * 65536); 
         db = "Ótimo, data correta. Vamos gerar a senha!!! 😉"              
     } 
