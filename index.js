58f993f2c0f6f4f33fb5b8c950c6a73a914dfe80
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-03-23 10:56:03 -0300

    incluindo variavel data na API

--- index.js
+++ index.js
@@ -20,8 +20,11 @@ app.post('/poc', (req, res) => {
 
     let numero = req.body.ns;
     let dataSistem = req.body.data;
-    let dataBios = dataSistem.replace(/[/]/g, "");
 
+    if (dataSistem != ""){
+        let dataBios = dataSistem.replace(/[/]/g, "");
+    }
+    
     let arr = [
         {
             "ns": "BCKD20000700G",
