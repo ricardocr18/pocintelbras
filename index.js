2668bce08aa716b7c49a17478dbd749368a5d5f4
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-03-24 08:56:32 -0300

    incluindo variavel data na API

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.post('/poc', (req, res) => {
                 db = `Ótimo. A data <span style="color:#00852b;"><b>${dataBios}</b></span> está correta. Vamos gerar a senha! 😉`
             }else{
                 db = `Infelizmente a data informada <span style="color:#00852b;"><b>${dataBios}</b></span> não corresponde com o número de série <span style="color:#00852b;"><b>${produto.ns}</b></span> do equipamento <span style="color:#00852b;"><b>${produto.modelo}</b></span>.`
-                dataBios === 'undefined'
+                numero === 'undefined'
             }
     }
 
